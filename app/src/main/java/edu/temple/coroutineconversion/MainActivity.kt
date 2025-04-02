ed7348ee8cf81237c3dd6c317ef94c6e22dd9f2c
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2025-04-02 15:31:59 -0400

    Refactor: Update Gradle and Android dependencies, convert to coroutines
    - Updated Gradle wrapper to version 8.10.2.
    - Updated Android application plugin to version 8.8.0.
    - Replaced the Thread and Handler code with coroutines for image opacity animation in `MainActivity.kt`.
    - Added a job to manage the scope of the coroutines in `MainActivity.kt`
    - Added the following config files: `.idea/runConfigurations.xml`, `.idea/vcs.xml`, and `.idea/deploymentTargetSelector.xml`.

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -1,17 +1,19 @@
 package edu.temple.coroutineconversion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-
-    //TODO (Refactor to replace Thread code with coroutines)
 
     private val cakeImageView: ImageView by lazy {
         findViewById(R.id.imageView)
@@ -21,24 +23,32 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
+    private val job = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + job)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            coroutineScope.launch {
+                revealCake()
+            }
         }
+    }
+
+    private suspend fun revealCake() {
+        for (i in 0 until 100) {
+            withContext(Dispatchers.Main) {
+                currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", i)
+                cakeImageView.alpha = i / 100f
+            }
+            delay(40)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
